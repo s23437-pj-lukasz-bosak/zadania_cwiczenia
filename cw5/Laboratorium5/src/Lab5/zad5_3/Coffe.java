697a3e2f5c73b0a4906609d6cadc9c875dd58fb6
s23437-pj-lukasz-bosak <73651774+s23437-pj-lukasz-bosak@users.noreply.github.com>
2022-05-28 16:34:38 +0200

    add coffes list and filled it.

---
+++ cw5/Laboratorium5/src/Lab5/zad5_3/Coffe.java
@@ -0,0 +1,22 @@
+package Lab5.zad5_3;
+
+import java.util.ArrayList;
+
+public class Coffe {
+
+    public static void main(String[] args) {
+        ArrayList <String> coffes= new ArrayList<String>();
+        coffes.add("espresso");
+        coffes.add("latte");
+        coffes.add("cappucino");
+        coffes.add("mocha");
+        coffes.add("auLait");
+        coffes.add("macchiato");
+
+        System.out.println(coffes);
+
+
+
+    }
+
+}
