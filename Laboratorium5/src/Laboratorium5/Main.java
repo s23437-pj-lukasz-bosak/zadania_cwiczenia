570473cdbc36b55ea066d6a977603f137ceade6a
s23437-pj-lukasz-bosak <73651774+s23437-pj-lukasz-bosak@users.noreply.github.com>
2022-05-28 13:56:47 +0200

    dodano interfejs

---
+++ Laboratorium5/src/Laboratorium5/Main.java
@@ -0,0 +1,11 @@
+package Laboratorium5;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        for ( int i = 0 ; i < 50 ; i++){
+
+        }
+    }
+}
