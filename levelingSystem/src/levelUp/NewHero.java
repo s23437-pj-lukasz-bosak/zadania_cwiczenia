e8a11c20ceb01934eb3e4694f708271a189ae1b2
s23437-pj-lukasz-bosak <73651774+s23437-pj-lukasz-bosak@users.noreply.github.com>
2022-06-23 09:00:25 +0200

    commit2

--- levelingSystem/src/levelUp/NewHero.java
+++ levelingSystem/src/levelUp/NewHero.java
@@ -5,6 +5,61 @@ import java.util.Scanner;
 public class NewHero {
     Scanner skaner = new Scanner(System.in);
 
+    private int strenght;
+    private int defence;
+    private int intelligence;
+    private int dexterity;
+    private int agility;
+    private int speed;
+
+
+    public int getStrenght() {
+        return strenght;
+    }
+
+    public void setStrenght(int strenght) {
+        this.strenght = strenght;
+    }
+
+    public int getDefence() {
+        return defence;
+    }
+
+    public void setDefence(int defence) {
+        this.defence = defence;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    public int getDexterity() {
+        return dexterity;
+    }
+
+    public void setDexterity(int dexterity) {
+        this.dexterity = dexterity;
+    }
+
+    public int getAgility() {
+        return agility;
+    }
+
+    public void setAgility(int agility) {
+        this.agility = agility;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
 
 
     public int heroClassChoose() {
@@ -23,15 +78,39 @@ public class NewHero {
             if (choose < 1 || choose > 3) {
                 System.out.println("Wybierz pomiedzy 1 -3");
             } else {
+                System.out.println(choose);
                 switch (choose) {
                     case 1:
                         System.out.println("wybrałes Mage");
+                        setDefence(10);
+                        setDexterity(10);
+                        setSpeed(5);
+                        setIntelligence(20);
+                        setStrenght(5);
+                        setAgility(10);
+                        break;
                     case 2:
                         System.out.println("wybrałes Knight");
+                        setDefence(10);
+                        setDexterity(5);
+                        setSpeed(10);
+                        setIntelligence(5);
+                        setStrenght(20);
+                        setAgility(10);
+                        break;
                     case 3:
                         System.out.println("wybrałes Archer");
-                        return choose;
+                        setDefence(5);
+                        setDexterity(20);
+                        setSpeed(10);
+                        setIntelligence(10);
+                        setStrenght(5);
+                        setAgility(10);
+                        break;
+
                 }
+
+                return choose;
             }
         }
 
--- levelingSystem/src/levelUp/Test.java
+++ levelingSystem/src/levelUp/Test.java
@@ -2,6 +2,17 @@ package levelUp;
 
 public class Test {
 
+    public static void main(String[] args) {
+
+        NewHero hero1 = new NewHero();
+        hero1.heroClassChoose();
+        System.out.println("twoje statystyki: siła = " + hero1.getStrenght()
+                + " , celność =  " + hero1.getDexterity() + " , inteligencja = " + hero1.getIntelligence() + " , obrona = " + hero1.getDefence()
+                    + " , agility = " + hero1.getAgility() + " , prędkość = " + hero1.getSpeed());
+
+
+
+    }
 
 
 }
